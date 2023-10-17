41caf01560117f6a1912c14d7cee0a787347f571
Alden Metzmaker <aldenmetzmaker@oakland.edu>
2023-10-16 23:43:24 -0400

    wip: extracting youtube video id

--- src/background.js
+++ src/background.js
@@ -1,2 +1,37 @@
 console.log('Hello Background');
-export {}
+
+function extractVideoIdAndValidate(url) {
+    const youtubeUrlPattern = /^https:\/\/(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)$/;
+    const match = url.match(youtubeUrlPattern);
+
+    if (match) {
+        // Valid YouTube URL, return ID
+        return match[1];
+    } else {
+        // Invalid, url return null
+        return null;
+    }
+}
+
+const url = "https://www.youtube.com/watch?v=VNmdw9ZNr1I";
+const videoId = extractVideoIdAndValidate(url);
+console.log("url1", url);
+if (videoId) {
+    console.log("Valid YouTube URL. Video ID: " + videoId);
+} else {
+    console.log("Invalid YouTube URL.");
+}
+
+const url2 = "https://www.nopeeee.com/watch?v=aySX3E9txVw";
+const videoId2 = extractVideoIdAndValidate(url2);
+console.log("url2", url2);
+
+if (videoId2) {
+    console.log("Valid YouTube URL. Video ID: " + videoId);
+} else {
+    console.log("Invalid YouTube URL.");
+}
+
+
+
+export { }
