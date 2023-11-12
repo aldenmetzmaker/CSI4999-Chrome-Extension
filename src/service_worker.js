6bfb8da3af33dfe79f02727f72b549c76cbb550c
Alden Metzmaker <aldenmetzmaker@oakland.edu>
2023-11-11 23:13:18 -0500

    wip: storing values and setting frontend state

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,4 +1,3 @@
-import youtube from './api/youtube.js'
 const KEY = "AIzaSyCudj37bf7-uNT8tYA1GscfijHf-dRlfs4";
 // //Extract YouTube tags from API result snippet (We can also get the Video Title here):
 
@@ -13,40 +12,42 @@ const KEY = "AIzaSyCudj37bf7-uNT8tYA1GscfijHf-dRlfs4";
 // //the app determines that an error occured)
 // TODO need to store the videoId in all cases. Right now only on next video/page visit
 // need to also save when tab is revisited when left playing
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.requestCurrentURL) {
-        chrome.scripting.executeScript({
-            target: { tabId: sender.tab.id },
-            function: (url) => {
-                return window.location.href;
-            },
-        }, (result) => {
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError);
-                return;
-            }
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//     if (message.requestCurrentURL) {
+//         chrome.scripting.executeScript({
+//             target: { tabId: sender.tab.id },
+//             function: (url) => {
+//                 return window.location.href;
+//             },
+//         }, (result) => {
+//             if (chrome.runtime.lastError) {
+//                 console.error(chrome.runtime.lastError);
+//                 return;
+//             }
 
-            const currentURL = result[0].result;
-            console.log("Current URL: " + currentURL);
-        });
-    }
-});
+//             const currentURL = result[0].result;
+//             console.log("Current URL: " + currentURL);
+//         });
+//     }
+// });
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+    console.log("hit")
     if (details.url) {
         const url = new URL(details.url);
         const videoId = url.searchParams.get("v");
         if (videoId) {
             console.log("Video ID has changed to: " + videoId);
-            chrome.storage.sync.set({ videoId }, function () {
+            chrome.storage.local.set({ videoId: videoId }).then(() => {
                 console.log("Video ID has been stored.");
-            });
+              });
 
             fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet&key=${KEY}`)
                 .then(response => response.json())
                 .then(data => {
-                    chrome.storage.sync.set({ response: data }, function () {
-                        console.log("API response has been stored.", data);
-                    });
+                    chrome.storage.local.set({ apiResponse: data.items }).then(() => {
+                        console.log("Response has been stored.", data.items);
+                      });
+                      chrome.runtime.sendMessage({ data });
                 })
                 .catch(error => {
                     console.error("Error fetching data:", error);
--- src/views/Popup/App.js
+++ src/views/Popup/App.js
@@ -1,25 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import ResultsCard from '../components/ResultsCard'
 import youtube from '../../api/youtube.js'
 
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     videos: [],
     loading: false,
-    videoId: null
+    videoId: null,
+    videoData: null,
   }
-  componentDidMount() {
-    chrome.storage.sync.get(["videoId"], function(result) {
-      const videoId = result.videoId;
-      console.log("Retrieved video ID: " + videoId);
-      
-      this.setState({ videoId: result.videoId });
-    }.bind(this));
+  async componentDidMount() {
+    // Retrieve videoId from chrome.storage.local
+    chrome.storage.local.get(["videoId"], (result) => {
+      const videoIdTemp = result.videoId;
+      console.log("Retrieved video ID:", videoIdTemp);
+  
+      // Set videoId state
+      this.setState({ videoId: videoIdTemp }, () => {
+        console.log("Updated videoId state:", this.state.videoId);
+      });
+    });
+  
+    // Listen for messages from the background script
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (message.data) {
+        // Set videoData state when a message is received
+        this.setState({ videoData: message.data }, () => {
+          console.log("Updated videoData state:", this.state.videoData);
+        });
+      }
+    });
+  
+    // Retrieve videoData from chrome.storage.local
+    chrome.storage.local.get("apiResponse", (result) => {
+      const videoDataTemp = result.apiResponse;
+      console.log('Retrieved videoData from storage:', videoDataTemp);
+  
+      // Set videoData state
+      this.setState({ videoData: videoDataTemp }, () => {
+        console.log("Updated videoData state:", this.state.videoData);
+      });
+    });
   }
   handleSubmit = async () => {
-    this.setState({termFromSearchBar: "vue 3 skills"})
     const response = await youtube.get('/videos', {
       params: {
         id: this.state.videoId
@@ -32,37 +57,37 @@ class App extends React.Component{
     console.log(response.data.items);
   }
 
-render () {
-  return (
-    <div className='popup'>
-      <div className='search-wrapper'>
-        <h2 className='title-text text--lg'>(Topic Name Here)</h2>
-        <button onClick={this.handleSubmit}>
-          console log video data
-        </button>
-        {!this.state.loading ? (<div className='results'>
-          <ResultsCard
-            title='Sample Title'
-            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
-          />
-          <ResultsCard
-            title='Sample Title'
-            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
-          />
-          <ResultsCard
-            title='Sample Title'
-            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
-          />
-          <ResultsCard
-            title='Sample Title'
-            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
-          />
-        </div>) : ('insert loading indicator here')}
+  render () {
+    return (
+      <div className='popup'>
+        <div className='search-wrapper'>
+          <h2 className='title-text text--lg'>(Topic Name Here)</h2>
+          <button onClick={this.handleSubmit}>
+            console log video data
+          </button>
+          {!this.state.loading ? (<div className='results'>
+            <ResultsCard
+              title='Sample Title'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
+            />
+            <ResultsCard
+              title='Sample Title'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
+            />
+            <ResultsCard
+              title='Sample Title'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
+            />
+            <ResultsCard
+              title='Sample Title'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
+            />
+          </div>) : ('insert loading indicator here')}
 
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default App;
