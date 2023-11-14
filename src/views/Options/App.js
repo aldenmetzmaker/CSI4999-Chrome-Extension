f60f3d2ddedadd1248c6ac35b0eb3e0c3d0b1b6e
Alden Metzmaker <aldenmetzmaker@oakland.edu>
2023-11-14 00:04:02 -0500

    feat: todos for development of options page

--- src/views/Options/App.js
+++ src/views/Options/App.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 
+// TODO - Need to create react componenets for varius parts of this page
+// place component files into src/components and import them. reference the resultsCard component for component setup
+// store user settings to chrome.storage
+// fetch stored data and set react state
+// user settings for theme, default prompt questions - other
+// create a way to apply css styling for multiple user input themes
+// the example now with toggle theme shows how to dynamically set css classes
 function App() {
+  // state variable declaration [variableName, set fucntion]
   const [theme, setTheme] = useState('dark');
 
+  // gets called when input element is changed, and updates state. this dynamically sets the css class
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
