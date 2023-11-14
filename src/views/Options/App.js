44f387112ad13518475dd911bfe38f6e593e5f3e
Alden Metzmaker <aldenmetzmaker@oakland.edu>
2023-11-14 00:09:30 -0500

    fix: typo lol

--- src/views/Options/App.js
+++ src/views/Options/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// TODO - Need to create react componenets for varius parts of this page
+// TODO - Need to create react componenets for various parts of this page
 // place component files into src/components and import them. reference the resultsCard component for component setup
 // store user settings to chrome.storage
 // fetch stored data and set react state
@@ -9,7 +9,7 @@ import './App.css';
 // create a way to apply css styling for multiple user input themes
 // the example now with toggle theme shows how to dynamically set css classes
 function App() {
-  // state variable declaration [variableName, set fucntion]
+  // state variable declaration [variableName, set function]
   const [theme, setTheme] = useState('dark');
 
   // gets called when input element is changed, and updates state. this dynamically sets the css class
