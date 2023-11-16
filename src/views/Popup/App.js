be8c1b52fa927c7a27c3f450fcd508a16643746e
Catt McHale <91563580+thealyx0ne@users.noreply.github.com>
2023-11-16 17:42:54 -0500

    Update App.js

--- src/views/Popup/App.js
+++ src/views/Popup/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import ResultsCard from '../components/ResultsCard'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ResultsCard from "../components/ResultsCard";
 // import youtube from '../../api/youtube.js' <- replacing with requests from service worker
 
 class App extends React.Component {
@@ -10,7 +10,7 @@ class App extends React.Component {
     videoId: null,
     videoData: null,
     videoTitle: null,
-  }
+  };
   // will eventually remove the console logs, but are there now for testing
   // view them by inspecting the popup
   async componentDidMount() {
@@ -18,35 +18,55 @@ class App extends React.Component {
     chrome.storage.local.get(["videoId"], (result) => {
       const videoIdTemp = result.videoId;
       console.log("Retrieved video ID:", videoIdTemp);
-  
+
       // Set videoId state
       this.setState({ videoId: videoIdTemp }, () => {
         console.log("Updated videoId state:", this.state.videoId);
       });
     });
-  
+
     // Listen for messages from the background script
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       if (message.data) {
         // Set videoData state when a message is received
-        this.setState({ videoData: message.data, videoTitle: message.data.items[0].snippet.title }, () => {
-          console.log("Updated videoData state:", this.state.videoData, this.state.videoTitle);
-        });
+        this.setState(
+          {
+            videoData: message.data,
+            videoTitle: message.data.items[0].snippet.title,
+          },
+          () => {
+            console.log(
+              "Updated videoData state:",
+              this.state.videoData,
+              this.state.videoTitle
+            );
+          }
+        );
       }
     });
-  
+
     // Retrieve videoData from chrome.storage.local
     chrome.storage.local.get("apiResponse", (result) => {
       const videoDataTemp = result.apiResponse;
-      console.log('Retrieved videoData from storage:', videoDataTemp);
-  
+      console.log("Retrieved videoData from storage:", videoDataTemp);
+
       // Set videoData, videoTitle state
-      this.setState({ videoData: videoDataTemp, videoTitle: videoDataTemp[0].snippet.title }, () => {
-        console.log("Updated videoData, videoTitle state:", this.state.videoData, this.state.videoTitle);
-      });
+      this.setState(
+        {
+          videoData: videoDataTemp,
+          videoTitle: videoDataTemp[0].snippet.title,
+        },
+        () => {
+          console.log(
+            "Updated videoData, videoTitle state:",
+            this.state.videoData,
+            this.state.videoTitle
+          );
+        }
+      );
     });
   }
-  // TODO may need function in the future 
+  // TODO may need function in the future
   handleSubmit = async () => {
     // probably will replace with a message to service worker, and handle requests from there
     // const response = await youtube.get('/videos', {
@@ -59,35 +79,43 @@ class App extends React.Component {
     //   videos: response.data.items,
     // })
     // console.log(this.state.videos);
-  }
+  };
 
-  render () {
+  render() {
     return (
-      <div className='popup'>
-        <div className='search-wrapper'>
-          <h2 className='title-text text--lg'>{this.state.videoTitle}</h2>
-          {!this.state.loading ? (<div className='results'>
+      <div className="popup">
+        <header className="popup-header">
+          <h1> Click Search - A React Extension</h1>
+          <button className="options-page-button">
+            {" "}
+            <a href="src\views\Options\App.css">Options</a>
+          </button>
+        </header>
+        <h2 className="title-text">{this.state.videoTitle}</h2>
+        {!this.state.loading ? (
+          <div className="results">
             <ResultsCard
-              title='Sample Title'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
+              title="Sample Title"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum."
             />
             <ResultsCard
-              title='Sample Title'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
+              title="Sample Title"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum."
             />
             <ResultsCard
-              title='Sample Title'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
+              title="Sample Title"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum."
             />
             <ResultsCard
-              title='Sample Title'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum.'
+              title="Sample Title"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis nec mauris commodo interdum."
             />
-          </div>) : ('TODO insert loading indicator here')}
-
-        </div>
+          </div>
+        ) : (
+          "TODO insert loading indicator here"
+        )}
       </div>
-    )
+    );
   }
 }
 
