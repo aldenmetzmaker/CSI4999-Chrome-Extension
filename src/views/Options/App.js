92b0cbf62196e4d4e934ace1f4925c5d16b31ade
Alden Metzmaker <aldenmetzmaker@oakland.edu>
2023-11-20 23:22:47 -0500

    wip: options page updates from demo

--- src/views/Options/App.js
+++ src/views/Options/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import QuestionInputForm from '../components/QuestionInputForm'
 
 // TODO - Need to create react componenets for various parts of this page
 // place component files into src/components and import them. reference the resultsCard component for component setup
@@ -12,27 +13,71 @@ import "./App.css";
 function App() {
   // state variable declaration [variableName, set function]
   const [theme, setTheme] = useState("dark");
+  const [showSaved, setShowSaved] = useState(false);
+  const [formToShow, setFormToShow] = useState('');
   const [preferredQuestions, setPreferredQuestions] = useState({
-    questionOne: '',
-    questionTwo: '',
-    questionThree: '',
-    questionFour: '',
-    questionFive: '',
+    peopleQuestions: {
+      questionOne: '',
+      questionTwo: '',
+      questionThree: '',
+      questionFour: '',
+      questionFive: '',
+    },
+    movieQuestions: {
+      questionOne: '',
+      questionTwo: '',
+      questionThree: '',
+      questionFour: '',
+      questionFive: '',
+    },
+    videoGameQuestions: {
+      questionOne: '',
+      questionTwo: '',
+      questionThree: '',
+      questionFour: '',
+      questionFive: '',
+    },
+    techQuestions: {
+      questionOne: '',
+      questionTwo: '',
+      questionThree: '',
+      questionFour: '',
+      questionFive: '',
+    },
+    anyQuestions: {
+      questionOne: '',
+      questionTwo: '',
+      questionThree: '',
+      questionFour: '',
+      questionFive: '',
+    },
+    historyQuestions: {
+      questionOne: '',
+      questionTwo: '',
+      questionThree: '',
+      questionFour: '',
+      questionFive: '',
+    }
   });
   useEffect(() => {
     // Fetch data from storage and set preferredQuestions state
     chrome.storage.local.get(['preferredQuestions'], (result) => {
-      const storedQuestions = result.preferredQuestions || {};
+      const storedQuestions = result.preferredQuestions || null;
       console.log(result)
-      setPreferredQuestions(storedQuestions);
+      if (storedQuestions) {
+        setPreferredQuestions(storedQuestions);
+      }
     });
   }, []);
-  const handleInputChange = (fieldName, event) => {
+  const handleInputChange = (section, fieldName, event) => {
     // Update the state with the new value
-    setPreferredQuestions({
-      ...preferredQuestions,
-      [fieldName]: event.target.value,
-    });
+    setPreferredQuestions((prevState) => ({
+      ...prevState,
+      [section]: {
+        ...prevState[section],
+        [fieldName]: event.target.value,
+      }
+    }))
   };
   // gets called when input element is changed, and updates state. this dynamically sets the css class
   const toggleTheme = () => {
@@ -43,16 +88,23 @@ function App() {
     chrome.storage.local.set({ preferredQuestions: preferredQuestions }, () => {
       console.log('Preferred questions set:', preferredQuestions);
     });
+    setShowSaved(true);
+    setTimeout(() => {
+      // Set showSaved back to false after 3 seconds
+      setShowSaved(false);
+    }, 3000);
   };
-  const clearQuestions = () => {
-    // clear values
-    setPreferredQuestions({
-      questionOne: '',
-      questionTwo: '',
-      questionThree: '',
-      questionFour: '',
-      questionFive: '',
-    })
+  const clearQuestions = (section) => {
+    setPreferredQuestions((prevState) => ({
+      ...prevState,
+      [section]: Object.fromEntries(
+        Object.keys(prevState[section]).map((key) => [key, ''])
+      ),
+    }));
+    console.log(preferredQuestions)
+  };
+  const showForm = (formName) => {
+    setFormToShow(formName);
   };
   return (
     <div className={`options ${theme}`}>
@@ -61,94 +113,69 @@ function App() {
       </header>
       <section className="theme-form">
         <h1 className={`section-title ${theme}`}>Theme</h1>
-        <label className="switch">
-          <input type="checkbox" onChange={toggleTheme} />
-          <span className="slider round"></span>
-        </label>
+        <span className="filter-item" onClick={toggleTheme}>{theme}</span>
       </section>
       <section className="options-prompts">
         <h2>Configure Prompts</h2>
         <section className="prompts-pre-set">
           <h3>Filter by Category:</h3>
-          <section className="prompts-input">
-            <input type="radio" id="people"></input>
-            <label for="people">People</label>
-            <input type="radio" id="movies-tv"></input>
-            <label for="movies-tv">Movies/TV</label>
-            <input type="radio" id="technology"></input>
-            <label for="technology">Technology</label>
-            <input type="radio" id="video-games"></input>
-            <label for="video-games">Video Games</label>
-            <input type="radio" id="history"></input>
-            <label for="history">History</label>
-            <input type="radio" id="any"></input>
-            <label for="any">Any</label>
-          </section>
+          <span onClick={() => showForm('people')} className="filter-item">People</span>
+          <span onClick={() => showForm('videoGames')} className="filter-item">Video Games</span>
+          <span onClick={() => showForm('movies')} className="filter-item">Movies/TV</span>
+          <span onClick={() => showForm('any')} className="filter-item">Any</span>
+          <span onClick={() => showForm('tech')} className="filter-item">Technology</span>
         </section>
-        <section className="prompts-user-set">
-          <h2>Set Custom Prompts:</h2>
-          <div>
-            <div className="input-wrapper">
-              <label htmlFor={'questionOne'} className="prompt-input-label">Preferred Question 1:</label>
-              <input
-                type="text"
-                id={'questionOne'}
-                value={preferredQuestions.questionOne}
-                placeholder={preferredQuestions.questionOne.length > 1 ? '' : 'Question 1'}
-                onChange={(event) => handleInputChange('questionOne', event)}
-                className="prompt-input"
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor={'questionTwo'} className="prompt-input-label">Preferred Question 2:</label>
-              <input
-                type="text"
-                id={'questionTwo'}
-                value={preferredQuestions.questionTwo}
-                placeholder={preferredQuestions.questionTwo.length > 1 ? '' : 'Question 2'}
-                onChange={(event) => handleInputChange('questionTwo', event)}
-                className="prompt-input"
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor={'questionThree'} className="prompt-input-label">Preferred Question 3:</label>
-              <input
-                type="text"
-                id={'questionThree'}
-                value={preferredQuestions.questionThree}
-                placeholder={preferredQuestions.questionThree.length > 1 ? '' : 'Question 3'}
-                onChange={(event) => handleInputChange('questionThree', event)}
-                className="prompt-input"
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor={'questionFour'} className="prompt-input-label">Preferred Question 4:</label>
-              <input
-                type="text"
-                id={'questionFour'}
-                value={preferredQuestions.questionFour}
-                placeholder={preferredQuestions.questionFour.length > 1 ? '' : 'Question 4'}
-                onChange={(event) => handleInputChange('questionFour', event)}
-                className="prompt-input"
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor={'questionFive'} className="prompt-input-label">Preferred Question 5:</label>
-              <input
-                type="text"
-                id={'questionFive'}
-                value={preferredQuestions.questionFive}
-                placeholder={preferredQuestions.questionFive.length > 1 ? '' : 'Question 5'}
-                onChange={(event) => handleInputChange('questionFive', event)}
-                className="prompt-input"
-              />
-            </div>
-            <div className="button-container">
-              <button className="button-main submit" onClick={saveQuestions}>Submit</button>
-              <button className="button-main clear" onClick={clearQuestions}>Clear</button>
-            </div>
-          </div>
-        </section>
+        {formToShow === 'people' && (
+        <QuestionInputForm
+          preferredQuestions={preferredQuestions.peopleQuestions}
+          handleInputChange={handleInputChange}
+          saveQuestions={saveQuestions}
+          clearQuestions={clearQuestions}
+          sectionName={'peopleQuestions'}
+          title={'People'}
+        />
+        )}
+        {formToShow === 'tech' && (
+        <QuestionInputForm
+          preferredQuestions={preferredQuestions.techQuestions}
+          handleInputChange={handleInputChange}
+          saveQuestions={saveQuestions}
+          clearQuestions={clearQuestions}
+          sectionName={'techQuestions'}
+          title={'Technology'}
+        />
+        )}
+        {formToShow === 'movies' && (
+        <QuestionInputForm
+          preferredQuestions={preferredQuestions.movieQuestions}
+          handleInputChange={handleInputChange}
+          saveQuestions={saveQuestions}
+          clearQuestions={clearQuestions}
+          sectionName={'movieQuestions'}
+          title={'Movies/TV'}
+        />
+        )}
+        {formToShow === 'videoGames' && (
+        <QuestionInputForm
+          preferredQuestions={preferredQuestions.videoGameQuestions}
+          handleInputChange={handleInputChange}
+          saveQuestions={saveQuestions}
+          clearQuestions={clearQuestions}
+          sectionName={'videoGameQuestions'}
+          title={'Video Games'}
+        />
+        )}
+        {formToShow === 'any' && (
+        <QuestionInputForm
+          preferredQuestions={preferredQuestions.anyQuestions}
+          handleInputChange={handleInputChange}
+          saveQuestions={saveQuestions}
+          clearQuestions={clearQuestions}
+          sectionName={'anyQuestions'}
+          title={'Any'}
+        />
+        )}
+        {showSaved ? (<span>Saved successfully</span>) : ('')}
       </section>
     </div>
   );
