714a67cd511c415cbb132200af828b11809a04be
Garrett-Pascoe <148263053+Garrett-Pascoe@users.noreply.github.com>
2023-12-11 21:35:17 -0500

    Update service_worker.js
    Added the complete tag algorithm.

--- src/service_worker.js
+++ src/service_worker.js
@@ -11,13 +11,6 @@ const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 // //Update Pop Up fields with the results of the query (possibly offer a search bar if 
 // //the app determines that an error occured)
 
-
-// TODO need to store the videoId in all cases. Right now only on next video/page visit
-// need to also save when tab is revisited when left playing
-
-
-//Current Build
-//var time = 0;
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
   if (details.url) {
     const url = new URL(details.url);
@@ -34,28 +27,13 @@ chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
           chrome.storage.local.set({ "apiResponse": data.items }).then(() => {
           console.log("Response has been stored.", data.items);
           });
-          try {
-          chrome.runtime.sendMessage({ data });
-          }catch(err){
-            console.error("error sending message: ", err);
-          }
         })
+
         .then(keyTag => { 
           chrome.storage.local.get(["apiResponse"]).then((result) => {
-            // only call tagalgo if tags exist
-            if (result.apiResponse[0].snippet.tags){
-              keyTag = tagAlgorithm(result.apiResponse[0].snippet.tags, result.apiResponse[0].snippet.title);
-              console.log("Response from tagAlgorithm", keyTag);
-              chrome.storage.local.set({ "keywords": keyTag});
-              try {
-                chrome.runtime.sendMessage({ keyTag });
-                }catch(err){
-                  console.error("error sending message: ", err);
-                }
-            }
-            else {
-              // need to add functionality for when no tags are present. video title, video description? something to put it the openai request
-            }
+            keyTag = tagAlgorithm(result.apiResponse[0].snippet.tags, result.apiResponse[0].snippet.title, result.apiResponse[0].snippet.channelTitle, result.apiResponse[0].snippet.categoryId);         
+            console.log("Response from tagAlgorithm", keyTag);
+            chrome.storage.local.set({ "keywords": keyTag});
           })
         })
         .catch(error => {
@@ -66,24 +44,25 @@ chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
 });
 
 // add video title to parameters
-function tagAlgorithm(tagArray, title)
+function tagAlgorithm(tagArray, title, channel, categoryId)
 {
-  //Eliminate common meta tags and make a new string without them or empty their spots in the current array
+  //initialize the return array
 
-  //Use === to compare strings
+  let returnArray = [4];
 
-  //Tags to look for:
-  //Anime, Video Game, Movie, TV show
+  //Test if the video has tags
 
-  //Tags to remove:
-  //Trailer, Netflix,
-  
-  //Potentially remove tags that are smaller than 3 characters?
+  if (tagArray === undefined)
+  {
+    returnArray[0] = "empty";
+    returnArray[1] = "empty";
+    returnArray[2] = channel;
+    returnArray[3] = "n/a";
 
-  //console.log("Array Size: ", tagArray.length);
-  //console.log("Tag Array: ", tagArray);
+    return returnArray;
+  }
 
-  //Initialize the points value of each tag to zero
+  //Create an array to store points for each tag
 
   let pointArray = [tagArray.length];
 
@@ -92,6 +71,13 @@ function tagAlgorithm(tagArray, title)
     pointArray[i] = 0;
   }
 
+  //Create variables to store points for each category
+
+  var moviePoints = 0;
+  var tvPoints = 0;
+  var videogamePoints = 0;
+  var techPoints = 0;
+
   //Split each tag string into an array of word strings
 
   let wordArray = [tagArray.length];
@@ -99,11 +85,142 @@ function tagAlgorithm(tagArray, title)
   for (var i = 0; i < tagArray.length; i++)
   {
     wordArray[i] = tagArray[i].split(" ");
+
+    for (var j = 0; j < wordArray[i].length; j++)
+    {
+      wordArray[i][j] = wordArray[i][j].toLowerCase();
+    }
   }
 
   //Split title into an array of word strings
 
   let titleArray = title.split(" ");
+
+  for (var i = 0; i < titleArray.length; i++)
+  {
+    titleArray[i] = titleArray[i].toLowerCase();
+  }
+
+  //Section for determining the video category
+
+  //Check the categoryId of the video
+
+  if (categoryId == 1)
+  {
+    moviePoints = moviePoints + 20;
+  }
+  else if (categoryId == 20)
+  {
+    videogamePoints = videogamePoints + 100;
+  }
+  else if (categoryId == 2 || categoryId == 28)
+  {
+    techPoints = techPoints + 100;
+  }
+  else if (categoryId == 23 || categoryId == 27 || categoryId == 26 ||
+  categoryId == 25 || categoryId == 10 || categoryId == 29 || categoryId == 22
+  || categoryId == 15 || categoryId == 17 || categoryId == 19)
+  {
+    //Return only the name of the channel if the video is a category unrelated to our chosen categories and exit the algorithm early
+    returnArray[0] = "empty";
+    returnArray[1] = "empty";
+    returnArray[2] = channel;
+    returnArray[3] = "n/a";
+
+    return returnArray;
+  }
+
+  //Compare the video title with category keywords
+
+  for (var k = 0; k < titleArray.length; k++)
+  {
+    var titleWord = titleArray[k].toLowerCase();
+
+    if (titleWord == "tv" || titleWord == "anime" || titleWord == "animes" || titleWord == "season" || titleWord == "netflix" || titleWord == "crunchyroll" || titleWord == "funimation"
+    || titleWord == "show" || titleWord == "shows")
+    {
+      tvPoints = tvPoints + 5;
+    }
+    else if (titleWord == "movie" || titleWord == "movies" || titleWord == "blockbuster" || titleWord == "box" || titleWord == "office" || titleWord == "ticket" || titleWord == "weekend")
+    {
+      moviePoints = moviePoints + 5;
+    }
+    else if (titleWord == "xbox" || titleWord == "nintendo" || titleWord == "playstation" || titleWord == "game" || titleWord == "games" || titleWord == "play" || titleWord == "playing"
+    || titleWord == "gameplay"|| titleWord == "letsplay"  || titleWord == "let'splay" || titleWord == "playthrough" || titleWord == "rpg" || titleWord == "tutorial" || titleWord == "tutorial"
+    || titleWord == "multiplayer" || titleWord == "fps" || titleWord == "guide" || titleWord == "secrets")
+    {
+      videogamePoints = videogamePoints + 5;
+    }
+    else if (titleWord == "apple" || titleWord == "microsoft" || titleWord == "linux" || titleWord == "asus" || titleWord == "intel" || titleWord == "cisco" || titleWord == "dell"
+    || titleWord == "adobe" || titleWord == "samsung" || titleWord == "iphone" || titleWord == "ram" || titleWord == "gigabytes" || titleWord == "terabytes" || titleWord == "processor"
+    || titleWord == "programming" || titleWord == "tech" || titleWord == "technology" || titleWord == "tutorial")
+    {
+      techPoints = techPoints + 5;
+    }
+  }
+
+  //Compare video tags with category keywords
+
+  for (var i = 0; i < tagArray.length; i++)
+  {
+    for (var j = 0; j < wordArray[i].length; j++)
+    {
+      var tagWord = wordArray[i][j].toLowerCase();
+
+      if (tagWord == "tv" || tagWord == "anime" || tagWord == "animes" || tagWord == "season" || tagWord == "netflix" || tagWord == "crunchyroll" || tagWord == "funimation"
+      || tagWord == "show" || tagWord == "shows")
+      {
+        tvPoints = tvPoints + 3;
+      }
+      else if (tagWord == "movie" || tagWord == "movies" || tagWord == "blockbuster" || tagWord == "box" || tagWord == "office" || tagWord == "ticket" || tagWord == "weekend")
+      {
+        moviePoints = moviePoints + 3;
+      }
+      else if (tagWord == "xbox" || tagWord == "nintendo" || tagWord == "playstation" || tagWord == "game" || tagWord == "games" || tagWord == "play" || tagWord == "playing"
+      || tagWord == "gameplay"|| tagWord == "letsplay"  || tagWord == "let'splay" || tagWord == "playthrough" || tagWord == "rpg" || tagWord == "tutorial" || tagWord == "tutorial"
+      || tagWord == "multiplayer" || tagWord == "fps" || tagWord == "guide" || tagWord == "secrets")
+      {
+        videogamePoints = videogamePoints + 3;
+      }
+      else if (tagWord == "apple" || tagWord == "microsoft" || tagWord == "linux" || tagWord == "asus" || tagWord == "intel" || tagWord == "cisco" || tagWord == "dell"
+      || tagWord == "adobe" || tagWord == "samsung" || tagWord == "iphone" || tagWord == "ram" || tagWord == "gigabytes" || tagWord == "terabytes" || tagWord == "processor"
+      || tagWord == "programming" || tagWord == "tech" || tagWord == "technology" || tagWord == "tutorial")
+      {
+        techPoints = techPoints + 3;
+      }
+    }
+  }
+
+  var highestCategory = 0;
+  returnArray[3] = "movie";     //Entertainment is defaulted to movie if no points were added
+
+  if (moviePoints > highestCategory)
+  {
+    highestCategory = moviePoints;
+    returnArray[3] = "movie";
+  }
+
+  if (tvPoints > highestCategory)
+  {
+    highestCategory = tvPoints;
+    returnArray[3] = "tv series";
+  }
+
+  if (videogamePoints > highestCategory)
+  {
+    highestCategory = videogamePoints;
+    returnArray[3] = "video game";
+  }
+
+  if (techPoints > highestCategory)
+  {
+    highestCategory = techPoints;
+    returnArray[3] = "tech";
+  }
+
+  //End of section for determining the video category
+
+  //Start of section for determining the most important tags
 
   //Compare strings to videotitle to find matching words
 
@@ -115,7 +232,7 @@ function tagAlgorithm(tagArray, title)
       {
         if (wordArray[i][j] === titleArray[k])
         {
-          pointArray[i] = pointArray[i] + 1;
+          pointArray[i] = pointArray[i] + 3;
         }
       }
     }
@@ -125,7 +242,6 @@ function tagAlgorithm(tagArray, title)
 
   for (var i = 0; i < tagArray.length; i++)
   {
-    //console.log("Iteration: ", wordArray[i]);
     for (var j = tagArray.length - i - 1; j > 0; j--)
     {
       var flag = 0;
@@ -137,7 +253,7 @@ function tagAlgorithm(tagArray, title)
           {
             if (wordArray[i][k] === wordArray[j][l] && flag < 2)
             {
-              if (wordArray[i].length > wordArray[j].length)
+              if (wordArray[i].length < wordArray[j].length)
               {
                 pointArray[i] = pointArray[i] + 1;
               }
@@ -158,17 +274,40 @@ function tagAlgorithm(tagArray, title)
 
   if (tagArray.length > 5)
   {
-    pointArray[0] = pointArray[0] + 3;
-    pointArray[1] = pointArray[1] + 2;
+    pointArray[0] = pointArray[0] + 5;
+    pointArray[1] = pointArray[1] + 3;
     pointArray[2] = pointArray[2] + 1;
     pointArray[tagArray.length - 1] = pointArray[tagArray.length - 1] + 1;
     pointArray[tagArray.length - 2] = pointArray[tagArray.length - 2] + 1;
     pointArray[tagArray.length - 3] = pointArray[tagArray.length - 3] + 1;
   }
 
+  //End of section for determining the most important tags
+
+  let channelArray = channel.split(" ");
+
+  for (var i = 0; i < channelArray.length; i++)
+  {
+    channelArray[i] = channelArray[i].toLowerCase();
+  }
+
+  for (var i = 0; i < tagArray.length; i++)
+  {
+    for (var j = 0; j < wordArray[i].length; j++)
+    {
+      for (var k = 0; k < channelArray.length; k++)
+      {
+        if (wordArray[i][j] === channelArray[k])
+        {
+          pointArray[i] = pointArray[i] - 3;
+        }
+      }
+    }
+  }
+
   //Evaluate the results and create a return list
 
-  //console.log("Points Array: ", pointArray);
+  //Initialize variables to store the 3 highest point values and their positions in the tag array
 
   var firstPlace = 0;
   var firstPosition = 0;
@@ -176,8 +315,9 @@ function tagAlgorithm(tagArray, title)
   var secondPosition = 0;
   var thirdPlace = 0;
   var thirdPosition = 0;
-  let returnArray = [3];
 
+
+  //Normal case with many tags
   if (tagArray.length > 2)
   {
     for (var i = 0; i < tagArray.length; i++)
@@ -204,11 +344,35 @@ function tagAlgorithm(tagArray, title)
         thirdPosition = i;
       }
     }
-    returnArray[0] = tagArray[firstPosition];
-    returnArray[1] = tagArray[secondPosition];
-    returnArray[2] = tagArray[thirdPosition];
+
+
+    //Shift the channel title to 3rd position of the array
+    if (firstPlace == channel)
+    {
+      firstPosition = thirdPosition
+
+      returnArray[0] = tagArray[firstPosition];
+      returnArray[1] = tagArray[secondPosition];
+      returnArray[2] = channel;
+    }
+    else if (secondPlace == channel)
+    {
+      secondPosition = thirdPosition
+
+      returnArray[0] = tagArray[firstPosition];
+      returnArray[1] = tagArray[secondPosition];
+      returnArray[2] = channel;
+    }
+    else
+    {
+      returnArray[0] = tagArray[firstPosition];
+      returnArray[1] = tagArray[secondPosition];
+      returnArray[2] = channel;
+    }
   }
-  else if (tagArray.length = 2)
+
+  //Case with only two tags
+  if (tagArray.length == 2)
   {
     if (pointArray[0] > pointArray[1])
     {
@@ -222,14 +386,16 @@ function tagAlgorithm(tagArray, title)
     }
     returnArray[0] = tagArray[firstPlace];
     returnArray[1] = tagArray[secondPlace];
-    returnArray[2] = "empty";
+    returnArray[2] = channel;
   }
-  else
+  else if (tagArray.length == 1)   //Case for a single tag
   {
-    returnArray[0] = tagArray[0];
-    returnArray[1] = "empty";
-    returnArray[2] = "empty";
+    returnArray[0] = "empty";
+    returnArray[1] = tagArray[0];
+    returnArray[2] = channel;
   }
+
+  console.log(pointArray);
 
   //return value
   return returnArray;
